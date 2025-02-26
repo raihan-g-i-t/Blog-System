16ce13b43e43c4fb14a665bcd32fad481af77ec3
raihank703 <raihank703@gmail.com>
2025-02-26 19:20:35 +0600

    26 feb

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,24 +7,29 @@
     <link rel="stylesheet "href="{{asset('css/login.css')}}">
 </head>
 <body>
-    <div class="login-container">
-        <h1>Login</h1>
-        <form action="" method="POST">
-            <label class="message">{{session('data') ?? ""}}</label>
-            @csrf
-            <div class="input-group">
-                <label for="email">Email</label>
-                <input type="text" id="email" name="email" placeholder="Enter your Email" required>
-            </div>
-            <div class="input-group">
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password" placeholder="Enter your password" required>
-            </div>
-            <button type="submit">Log In</button>
-        </form>
-        <div class="footer">
-            <p>Don't have an account? <a href="">Sign up</a></p>
+
+<div class="login-container">
+    <a href="{{ route('index') }}"><h3>Name</h3></a>
+</div>
+
+<div class="login-container">
+    <h1>Login</h1>
+    <form action="" method="POST">
+        <label class="message">{{session('data') ?? ""}}</label>
+        @csrf
+        <div class="input-group">
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email" placeholder="Enter your Email" required>
         </div>
+        <div class="input-group">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Enter your password" required>
+        </div>
+        <button type="submit">Log In</button>
+    </form>
+    <div class="footer">
+        <p>Don't have an account? <a href="{{ route('user.registration') }}">Sign up</a></p>
     </div>
+</div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'index');
+Route::view('/', 'index')->name('index');
 Route::view('/login','login')->name('login');
 Route::view('/user_registration','user.registration')->name('user.registration');
 
