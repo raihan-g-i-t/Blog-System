e22a3160ea58a185d5cb6c9821be7a29a3f6db88
raihan133 <opu.raihankhan@gmail.com>
2025-03-16 22:01:55 +0600

    14 mar (2)

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Http\Services\LoginService;
 
 class loginController extends Controller
 {
+    private $loginService;
+    public function __construct(){
+        $this->loginService = new LoginService();
+    }
+
     public function index(){
         return view('index');
     }
@@ -16,11 +22,13 @@ class loginController extends Controller
     }
 
     public function register_store(Request $request){
-        DB::table('users')->insert([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password)
+        
+        $this->loginService->user_registration($request);
+        // DB::table('users')->insert([
+        //     'name' => $request->name,
+        //     'email' => $request->email,
+        //     'password' => bcrypt($request->password)
 
-        ]);
+        // ]);
     }
 }
--- app/Http/services/loginService.php
+++ app/Http/services/loginService.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Services;
 
+use Illuminate\Support\Facades\DB;
 
 class LoginService{
 
-    
+    public function user_registration($data){
+        DB::table('users')->insert([
+            'name' => $data->name,
+            'email' => $data->email,
+            'password' => bcrypt($data->password)
+
+        ]);
+    }
 
 }
